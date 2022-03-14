ac60502c7da2ff80d9be11507621cc81ca494245
Luc Charbonneau <luc@coastbeachwear.com>
2022-03-14 01:35:42 -0400

    Added product rating model validation.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
     validates :name, presence: true
     validates :price, presence: true
-    validates :rating, presence: true
+    validates :rating, :inclusion => 1..5
+    
 
     has_and_belongs_to_many :carts
     has_one_attached :feature_image
